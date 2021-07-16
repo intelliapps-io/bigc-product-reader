13d7ccc3d0ec422cdcfb26f7b18c4981fb7ad621
Jared Moore <jared.moore@intelliapps.io>
2021-07-16 02:03:26 -0400

    parsed data and format into csv

---
+++ src/index.ts
@@ -0,0 +1,128 @@
+import fs from 'fs'
+import parse from 'csv-parse'
+import transform from 'stream-transform'
+import { BCFieldName, BCItemType, CLEntity } from './types'
+
+/** Parse Data from CSV */
+function parseData(FILE_PATH: string): Promise<object[]> {
+  return new Promise((resolve: (data: object[]) => void, reject: (err: Error) => void) => {
+    let output: object[] = []
+
+    const parser = parse({
+      delimiter: ',',
+      columns: true,
+      autoParse: true,
+    })
+
+    parser.on('readable', function () {
+      let record
+      while (record = parser.read() as any) {
+        output.push(record)
+      }
+    })
+
+    parser.on('error', function (err) { reject(err) })
+
+    parser.on('end', function () { resolve(output) })
+
+    fs.createReadStream(FILE_PATH).pipe(parser);
+  })
+}
+
+/** Create Clover Record from BigCommerce */
+function mapRecordToCLEntity(record: any, parentRecord?: any): CLEntity {
+  let entity: CLEntity = {
+    name: record[BCFieldName['Product Name']],
+    price: record[BCFieldName['Price']].trim().replace("[FIXED]", ""),
+    price_type: "Fixed",
+    cost: 0,
+    SKU: record[BCFieldName['Product Code/SKU']],
+    tax_rates: ['DEFAULT'],
+    quantity: 0
+  }
+
+  const parentName = parentRecord ? parentRecord[BCFieldName['Product Name']] : undefined
+  if (parentName) {
+    const splitChildName = record[BCFieldName['Product Name']]
+      .replace('[RT]', '')
+      .replace('[RB]', '')
+      .replace('[CS]', '')
+      .replace('[S]', '')
+      .replace(',', ', ')
+      .trim()
+
+    // set name
+    entity.name = `${parentName} [${splitChildName}]`
+
+    // carry price
+    if (!entity.price)
+      entity.price = parentRecord[BCFieldName['Price']]
+  }
+
+  return entity
+}
+
+async function writeCSV(data: CLEntity[], FILE_PATH: string): Promise<any> {
+  return new Promise((resolve: (data: any) => void, reject: (err: Error) => void) => {
+    let csv = ""
+    let output: any[][] = [
+      ["name", "price", "SKU", "price_type", "cost", "tax_rates", "quantity",]
+    ]
+    for (let i = 0; i < data.length; i++) {
+      const entity = data[i]
+      const row = [entity.name, entity.price, entity.SKU, entity.price_type, entity.cost, entity.tax_rates, entity.quantity]
+      output.push(row)
+    }
+
+    for (let i = 0; i < output.length; i++) {
+      const row = output[i]
+      let row_csv = ""
+      for (let j = 0; j < row.length; j++) {
+        const field = row[j]
+        row_csv += `${!field ? "" : `"${field}"`}${j - 1 === row.length ? "" : ","}`
+      }
+      csv += row_csv + "\n"
+    }
+
+    resolve(csv)
+  })
+}
+
+/** 
+ * Entry - MAIN
+ */
+async function main() {
+  const FILE_PATH = `${__dirname}/products.csv`;
+  const OUT_PATH = `${__dirname}/result.csv`;
+  const parsedData = await parseData(FILE_PATH)
+  let output: CLEntity[] = []
+
+  /** Map parsedData to CLEntity */
+  let parentRecord: object | undefined
+  for (let i = 0; i < parsedData.length; i++) {
+    let record = parsedData[i] as any
+    let entity: CLEntity
+
+    // set or reset parent record
+    if (record[BCFieldName['Item Type']] === BCItemType['Product']) {
+      parentRecord = undefined
+      entity = mapRecordToCLEntity(record, parentRecord)
+      if (entity.SKU)
+        output.push(entity)
+    }
+    else if (record[BCFieldName['Item Type']].trim() === BCItemType['SKU'] && typeof parentRecord === "undefined") {
+      parentRecord = parsedData[i - 1]
+      entity = mapRecordToCLEntity(record, parentRecord)
+      if (entity.SKU)
+        output.push(entity)
+    }
+  }
+
+  // console.log(output)
+  const result = await writeCSV(output, OUT_PATH)
+  console.log(result);
+}
+
+main()
+  .then(() => console.log("done"))
+  .catch((e) => console.error(e))
---
+++ src/types.ts
@@ -0,0 +1,107 @@
+/**
+ * BigCommerce 
+ */
+export enum BCItemType {
+  "Product" = "Product",
+  "SKU" = "SKU",
+  "Rule" = "Rule",
+}
+
+export enum BCFieldName {
+  "Item Type" = "Item Type",
+  "Product ID" = "Product ID",
+  "Product Name" = "Product Name",
+  "Product Type" = "Product Type",
+  "Product Code/SKU" = "Product Code/SKU",
+  "Bin Picking Number" = "Bin Picking Number",
+  "Brand Name" = "Brand Name",
+  "Option Set" = "Option Set",
+  "Option Set Align" = "Option Set Align",
+  "Product Description" = "Product Description",
+  "Price" = "Price",
+  "Cost Price" = "Cost Price",
+  "Retail Price" = "Retail Price",
+  "Sale Price" = "Sale Price",
+  "Fixed Shipping Cost" = "Fixed Shipping Cost",
+  "Free Shipping" = "Free Shipping",
+  "Product Warranty" = "Product Warranty",
+  "Product Weight" = "Product Weight",
+  "Product Width" = "Product Width",
+  "Product Height" = "Product Height",
+  "Product Depth" = "Product Depth",
+  "Allow Purchases?" = "Allow Purchases?",
+  "Product Visible?" = "Product Visible?",
+  "Product Availability" = "Product Availability",
+  "Track Inventory" = "Track Inventory",
+  "Current Stock Level" = "Current Stock Level",
+  "Low Stock Level" = "Low Stock Level",
+  "Category" = "Category",
+  "Product Image ID - 1" = "Product Image ID - 1",
+  "Product Image File - 1" = "Product Image File - 1",
+  "Product Image Description - 1" = "Product Image Description - 1",
+  "Product Image Is Thumbnail - 1" = "Product Image Is Thumbnail - 1",
+  "Product Image Sort - 1" = "Product Image Sort - 1",
+  "Product Image ID - 2" = "Product Image ID - 2",
+  "Product Image File - 2" = "Product Image File - 2",
+  "Product Image Description - 2" = "Product Image Description - 2",
+  "Product Image Is Thumbnail - 2" = "Product Image Is Thumbnail - 2",
+  "Product Image Sort - 2" = "Product Image Sort - 2",
+  "Search Keywords" = "Search Keywords",
+  "Page Title" = "Page Title",
+  "Meta Keywords" = "Meta Keywords",
+  "Meta Description" = "Meta Description",
+  "Product Condition" = "Product Condition",
+  "Show Product Condition?" = "Show Product Condition?",
+  "Sort Order" = "Sort Order",
+  "Product Tax Class" = "Product Tax Class",
+  "Product UPC/EAN" = "Product UPC/EAN",
+  "Stop Processing Rules" = "Stop Processing Rules",
+  "Product URL" = "Product URL",
+  "Redirect Old URL?" = "Redirect Old URL?",
+  "GPS Global Trade Item Number" = "GPS Global Trade Item Number",
+  "GPS Manufacturer Part Number" = "GPS Manufacturer Part Number",
+  "GPS Gender" = "GPS Gender",
+  "GPS Age Group" = "GPS Age Group",
+  "GPS Color" = "GPS Color",
+  "GPS Size" = "GPS Size",
+  "GPS Material" = "GPS Material",
+  "GPS Pattern" = "GPS Pattern",
+  "GPS Item Group ID" = "GPS Item Group ID",
+  "GPS Category" = "GPS Category",
+  "GPS Enabled" = "GPS Enabled",
+  "Tax Provider Tax Code" = "Tax Provider Tax Code",
+  "Product Custom Fields" = "Product Custom Fields",
+}
+
+/**
+ * Clover Field Name
+ */
+export enum CLFieldName {
+  "Clover ID" = "Clover ID",
+  "Name" = "Name",
+  "Price" = "Price",
+  "Price Type" = "Price Type",
+  "Price Unit" = "Price Unit",
+  "Tax Rates" = "Tax Rates",
+  "Cost" = "Cost",
+  "Product Code" = "Product Code",
+  "SKU" = "SKU",
+  "Modifier Groups" = "Modifier Groups",
+  "Quantity" = "Quantity",
+  "Labels" = "Labels",
+  "Hidden" = "Hidden",
+  "Non-revenue item" = "Non-revenue item",
+}
+
+export interface CLEntity {
+  clover_ID?: string
+  name: string
+  price: number
+  price_type: string
+  price_unit?: string
+  tax_rates: string[]
+  cost: number
+  product_code?: number
+  SKU: number
+  quantity: number
+}
